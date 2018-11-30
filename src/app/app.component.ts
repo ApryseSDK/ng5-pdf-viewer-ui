4648e34bd390fe46fbb756bea8e4782a78690ff0
Johan <jjohansson@pdftron.com>
2018-11-30 13:29:47 -0800

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,184 @@
+import { Component, ViewChild } from '@angular/core';
+
+import {
+  PDFProgressData,
+  PdfViewerComponent,
+  PDFDocumentProxy,
+  PDFSource,
+} from 'ng2-pdf-viewer';
+
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  pdfSrc: string | PDFSource | ArrayBuffer = '../assets/example.pdf';
+
+  error: any;
+  page = 1;
+  rotation = 0;
+  zoom = 1.0;
+  originalSize = true;
+  pdf: any;
+  renderText = true;
+  progressData: PDFProgressData;
+  isLoaded = false;
+  stickToPage = false;
+  showAll = true;
+  autoresize = true;
+  fitToPage = false;
+  outline: any[];
+  isOutlineShown = false;
+  pdfQuery = '';
+
+  @ViewChild(PdfViewerComponent) private pdfComponent: PdfViewerComponent;
+
+  openLocalFile() {
+    jQuery('#file').trigger('click');
+  }
+
+  toggleOutline() {
+    this.isOutlineShown = !this.isOutlineShown;
+  }
+
+  incrementPage(amount: number) {
+    this.page += amount;
+  }
+
+  incrementZoom(amount: number) {
+    this.zoom += amount;
+  }
+
+  rotate(angle: number) {
+    this.rotation += angle;
+  }
+
+  /**
+   * Render PDF preview on selecting file
+   */
+  onFileSelected() {
+    const $pdf: any = document.querySelector('#file');
+
+    if (typeof FileReader !== 'undefined') {
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        this.pdfSrc = e.target.result;
+      };
+
+      reader.readAsArrayBuffer($pdf.files[0]);
+    }
+  }
+
+  /**
+   * Get pdf information after it's loaded
+   * @param pdf
+   */
+  afterLoadComplete(pdf: PDFDocumentProxy) {
+    this.pdf = pdf;
+    this.isLoaded = true;
+
+    this.loadOutline();
+  }
+
+  /**
+   * Get outline
+   */
+  loadOutline() {
+    this.pdf.getOutline().then((outline: any[]) => {
+      this.outline = outline;
+    });
+  }
+
+  /**
+   * Handle error callback
+   *
+   * @param error
+   */
+  onError(error: any) {
+    this.error = error; // set error
+
+    if (error.name === 'PasswordException') {
+      const password = prompt(
+        'This document is password protected. Enter the password:'
+      );
+
+      if (password) {
+        this.error = null;
+        this.setPassword(password);
+      }
+    }
+  }
+
+  setPassword(password: string) {
+    let newSrc;
+    if (this.pdfSrc instanceof ArrayBuffer) {
+      newSrc = { data: this.pdfSrc };
+    } else if (typeof this.pdfSrc === 'string') {
+      newSrc = { url: this.pdfSrc };
+    } else {
+      newSrc = { ...this.pdfSrc };
+    }
+    newSrc.password = password;
+    this.pdfSrc = newSrc;
+  }
+
+  /**
+   * Pdf loading progress callback
+   * @param {PDFProgressData} progressData
+   */
+  onProgress(progressData: PDFProgressData) {
+    console.log(progressData);
+    this.progressData = progressData;
+    this.isLoaded = false;
+    this.error = null; // clear error
+  }
+
+  getInt(value: number): number {
+    return Math.round(value);
+  }
+
+  /**
+   * Navigate to destination
+   * @param destination
+   */
+  navigateTo(destination: any) {
+    this.pdfComponent.pdfLinkService.navigateTo(destination);
+  }
+
+  /**
+   * Scroll view
+   */
+  scrollToPage() {
+    this.pdfComponent.pdfViewer.scrollPageIntoView({
+      pageNumber: 3,
+    });
+  }
+
+  /**
+   * Page rendered callback, which is called when a page is rendered (called multiple times)
+   *
+   * @param {CustomEvent} e
+   */
+  pageRendered(e: CustomEvent) {
+    console.log('(page-rendered)', e);
+  }
+
+  searchQueryChanged(newQuery: string) {
+    if (newQuery !== this.pdfQuery) {
+      this.pdfQuery = newQuery;
+      this.pdfComponent.pdfFindController.executeCommand('find', {
+        query: this.pdfQuery,
+        highlightAll: true,
+      });
+    } else {
+      this.pdfComponent.pdfFindController.executeCommand('findagain', {
+        query: this.pdfQuery,
+        highlightAll: true,
+      });
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { InlineSVGModule } from 'ng-inline-svg';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    PdfViewerModule,
+    InlineSVGModule.forRoot(),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
